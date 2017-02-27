2427bfbecffefc1186d11d7047ee0b6f18948a52
Electric Engineer <493404216@qq.com>
2017-02-27 22:47:17 +0800

    2017-2-27 22:47:07

---
+++ MathClient.cs
@@ -0,0 +1,12 @@
+using System;
+namespace Wrox
+{
+    class Client    
+    {
+        public static void Main()
+        {
+            MathLib mathObj = new MathLib();
+            Console.WriteLine(mathObj.Add(7,8));
+        }
+    }
+}
